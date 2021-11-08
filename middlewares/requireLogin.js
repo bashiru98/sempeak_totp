c00a8927503d88595895bdd559fd221562645ab2
bashiru98 <atulebashirubukari@gmail.com>
2021-11-08 04:54:38 +0300

    added totp

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,6 +1,8 @@
+let fs = require('fs');
+
 module.exports = (req, res, next) => {
   if (!req.user) {
-    return res.status(401).send({ error: 'You must log in!' });
+   return  res.status(403).json("<html> <head>server Response</head><body><h1> Forbidden action</p></h1></body></html>");
   }
 
   next();
