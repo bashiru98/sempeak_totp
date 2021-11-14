55cf6c130b7493c13051d76d08e07a04d0873ae3
bashiru98 <atulebashirubukari@gmail.com>
2021-11-15 01:56:29 +0300

    added new commit10

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
+    keys: ['123123123']
   })
 );
 app.use(passport.initialize());
